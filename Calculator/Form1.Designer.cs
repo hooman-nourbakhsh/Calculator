5746b4a20e89f4030ce621e9cffe852dea40f72a
HooMaN <HooMaN@DESKTOP-235ANIT>
2018-12-19 00:16:54 +0330

    ساخت ورژن 1 نرم افزار
    Version : 1.0

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -28,12 +28,354 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnDiv = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnSub = new System.Windows.Forms.Button();
+            this.btnSum = new System.Windows.Forms.Button();
+            this.btnEqual = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btnDot = new System.Windows.Forms.Button();
+            this.btnClick = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btnOnOff = new System.Windows.Forms.Button();
+            this.txtDisplay = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.panel1.Controls.Add(this.btnDiv);
+            this.panel1.Controls.Add(this.btnMultiply);
+            this.panel1.Controls.Add(this.btnSub);
+            this.panel1.Controls.Add(this.btnSum);
+            this.panel1.Controls.Add(this.btnEqual);
+            this.panel1.Controls.Add(this.btn9);
+            this.panel1.Controls.Add(this.btn6);
+            this.panel1.Controls.Add(this.btn3);
+            this.panel1.Controls.Add(this.btnDot);
+            this.panel1.Controls.Add(this.btnClick);
+            this.panel1.Controls.Add(this.btn8);
+            this.panel1.Controls.Add(this.btn5);
+            this.panel1.Controls.Add(this.btn2);
+            this.panel1.Controls.Add(this.btn0);
+            this.panel1.Controls.Add(this.btnDel);
+            this.panel1.Controls.Add(this.btn7);
+            this.panel1.Controls.Add(this.btn4);
+            this.panel1.Controls.Add(this.btn1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Enabled = false;
+            this.panel1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.panel1.Location = new System.Drawing.Point(0, 76);
+            this.panel1.Margin = new System.Windows.Forms.Padding(5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(279, 213);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnDiv
+            // 
+            this.btnDiv.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDiv.Location = new System.Drawing.Point(212, 46);
+            this.btnDiv.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDiv.Name = "btnDiv";
+            this.btnDiv.Size = new System.Drawing.Size(61, 36);
+            this.btnDiv.TabIndex = 0;
+            this.btnDiv.Text = "/";
+            this.btnDiv.UseVisualStyleBackColor = true;
+            this.btnDiv.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Operators);
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMultiply.Location = new System.Drawing.Point(212, 89);
+            this.btnMultiply.Margin = new System.Windows.Forms.Padding(4);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(61, 36);
+            this.btnMultiply.TabIndex = 0;
+            this.btnMultiply.Text = "*";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Operators);
+            // 
+            // btnSub
+            // 
+            this.btnSub.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSub.Location = new System.Drawing.Point(212, 131);
+            this.btnSub.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSub.Name = "btnSub";
+            this.btnSub.Size = new System.Drawing.Size(61, 36);
+            this.btnSub.TabIndex = 0;
+            this.btnSub.Text = "-";
+            this.btnSub.UseVisualStyleBackColor = true;
+            this.btnSub.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Operators);
+            // 
+            // btnSum
+            // 
+            this.btnSum.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSum.Location = new System.Drawing.Point(212, 174);
+            this.btnSum.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSum.Name = "btnSum";
+            this.btnSum.Size = new System.Drawing.Size(61, 36);
+            this.btnSum.TabIndex = 0;
+            this.btnSum.Text = "+";
+            this.btnSum.UseVisualStyleBackColor = true;
+            this.btnSum.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Operators);
+            // 
+            // btnEqual
+            // 
+            this.btnEqual.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnEqual.ForeColor = System.Drawing.Color.Red;
+            this.btnEqual.Location = new System.Drawing.Point(144, 174);
+            this.btnEqual.Margin = new System.Windows.Forms.Padding(4);
+            this.btnEqual.Name = "btnEqual";
+            this.btnEqual.Size = new System.Drawing.Size(61, 36);
+            this.btnEqual.TabIndex = 0;
+            this.btnEqual.Text = "=";
+            this.btnEqual.UseVisualStyleBackColor = true;
+            this.btnEqual.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btnEqual_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn9.Location = new System.Drawing.Point(143, 46);
+            this.btn9.Margin = new System.Windows.Forms.Padding(4);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(61, 36);
+            this.btn9.TabIndex = 0;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Numbers);
+            // 
+            // btn6
+            // 
+            this.btn6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn6.Location = new System.Drawing.Point(143, 88);
+            this.btn6.Margin = new System.Windows.Forms.Padding(4);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(61, 36);
+            this.btn6.TabIndex = 0;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Numbers);
+            // 
+            // btn3
+            // 
+            this.btn3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn3.Location = new System.Drawing.Point(143, 131);
+            this.btn3.Margin = new System.Windows.Forms.Padding(4);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(61, 36);
+            this.btn3.TabIndex = 0;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Numbers);
+            // 
+            // btnDot
+            // 
+            this.btnDot.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDot.Location = new System.Drawing.Point(75, 174);
+            this.btnDot.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDot.Name = "btnDot";
+            this.btnDot.Size = new System.Drawing.Size(61, 36);
+            this.btnDot.TabIndex = 0;
+            this.btnDot.Text = ".";
+            this.btnDot.UseVisualStyleBackColor = true;
+            this.btnDot.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Numbers);
+            // 
+            // btnClick
+            // 
+            this.btnClick.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnClick.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.btnClick.ForeColor = System.Drawing.Color.Crimson;
+            this.btnClick.Location = new System.Drawing.Point(143, 4);
+            this.btnClick.Margin = new System.Windows.Forms.Padding(4);
+            this.btnClick.Name = "btnClick";
+            this.btnClick.Size = new System.Drawing.Size(130, 36);
+            this.btnClick.TabIndex = 0;
+            this.btnClick.Text = "Clear";
+            this.btnClick.UseVisualStyleBackColor = true;
+            // 
+            // btn8
+            // 
+            this.btn8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn8.Location = new System.Drawing.Point(74, 46);
+            this.btn8.Margin = new System.Windows.Forms.Padding(4);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(61, 36);
+            this.btn8.TabIndex = 0;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Numbers);
+            // 
+            // btn5
+            // 
+            this.btn5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn5.Location = new System.Drawing.Point(74, 88);
+            this.btn5.Margin = new System.Windows.Forms.Padding(4);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(61, 36);
+            this.btn5.TabIndex = 0;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Numbers);
+            // 
+            // btn2
+            // 
+            this.btn2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn2.Location = new System.Drawing.Point(74, 131);
+            this.btn2.Margin = new System.Windows.Forms.Padding(4);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(61, 36);
+            this.btn2.TabIndex = 0;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Numbers);
+            // 
+            // btn0
+            // 
+            this.btn0.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn0.Location = new System.Drawing.Point(6, 173);
+            this.btn0.Margin = new System.Windows.Forms.Padding(4);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(61, 36);
+            this.btn0.TabIndex = 0;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Numbers);
+            // 
+            // btnDel
+            // 
+            this.btnDel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.btnDel.Location = new System.Drawing.Point(6, 3);
+            this.btnDel.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(129, 36);
+            this.btnDel.TabIndex = 0;
+            this.btnDel.Text = "Back";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn7.Location = new System.Drawing.Point(6, 46);
+            this.btn7.Margin = new System.Windows.Forms.Padding(4);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(61, 36);
+            this.btn7.TabIndex = 0;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Numbers);
+            // 
+            // btn4
+            // 
+            this.btn4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn4.Location = new System.Drawing.Point(6, 88);
+            this.btn4.Margin = new System.Windows.Forms.Padding(4);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(61, 36);
+            this.btn4.TabIndex = 0;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Numbers);
+            // 
+            // btn1
+            // 
+            this.btn1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn1.Location = new System.Drawing.Point(6, 131);
+            this.btn1.Margin = new System.Windows.Forms.Padding(4);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(61, 36);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Numbers);
+            // 
+            // btnOnOff
+            // 
+            this.btnOnOff.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnOnOff.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOnOff.ForeColor = System.Drawing.Color.MediumBlue;
+            this.btnOnOff.Location = new System.Drawing.Point(6, 36);
+            this.btnOnOff.Margin = new System.Windows.Forms.Padding(4);
+            this.btnOnOff.Name = "btnOnOff";
+            this.btnOnOff.Size = new System.Drawing.Size(61, 36);
+            this.btnOnOff.TabIndex = 0;
+            this.btnOnOff.Text = "On";
+            this.btnOnOff.UseVisualStyleBackColor = true;
+            this.btnOnOff.Click += new System.EventHandler(this.btnOnOff_Click);
+            // 
+            // txtDisplay
+            // 
+            this.txtDisplay.BackColor = System.Drawing.Color.White;
+            this.txtDisplay.Dock = System.Windows.Forms.DockStyle.Top;
+            this.txtDisplay.Location = new System.Drawing.Point(0, 0);
+            this.txtDisplay.Margin = new System.Windows.Forms.Padding(4);
+            this.txtDisplay.Name = "txtDisplay";
+            this.txtDisplay.ReadOnly = true;
+            this.txtDisplay.Size = new System.Drawing.Size(279, 33);
+            this.txtDisplay.TabIndex = 1;
+            this.txtDisplay.TextChanged += new System.EventHandler(this.txtDisplay_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(279, 289);
+            this.Controls.Add(this.txtDisplay);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnOnOff);
+            this.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Button btnSub;
+        private System.Windows.Forms.Button btnSum;
+        private System.Windows.Forms.Button btnEqual;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btnDot;
+        private System.Windows.Forms.Button btnClick;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btnOnOff;
+        private System.Windows.Forms.Button btnDiv;
+        private System.Windows.Forms.TextBox txtDisplay;
     }
 }
 
--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -9,11 +9,182 @@ using System.Windows.Forms;
 
 namespace Calculator
 {
+    /// <summary>
+    /// این یک برنامه ماشین حساب معمولی هست که با استفاده از مهندسی نرم افزار سعی شده 
+    /// تا بصورت ساده ، بهینه و استفاده از کمترین حجم کد درمقابل بالاترین عملکرد را داشته باشیم.
+
+    /// Source: https://t.me/bytelearn
+    /// Creator:HooMaN (https://github.com/sharlobin2)
+    /// </summary>
     public partial class Form1 : Form
     {
+        /// <summary>
+        /// تعریف متغیر های سراسری
+        /// x: عدد اول
+        /// y: عدد دوم
+        /// z: حاصل
+        /// op: عملگرها
+        /// </summary>
+        double x, y, z;
+        string op;
+        Boolean flag;//استفاده از تکنیک پرچم
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Numbers(object sender, MouseEventArgs e)
+        {
+            //تابع ای برای تمام شماره ها
+            //مشخص می شود رویداد کلیک برای کدام دکمه فراخوانی شده است sender که توسط
+            if (flag == true)
+            {
+                //شد حافظه را پاک و پرچم را خاموش میکنیم true بعد از اینکه یکی از عملگرها وارد شد و پرچم مقدار 
+                txtDisplay.Text = "";
+                flag = false;
+            }
+            //سپس عدد دوم را دریافت می کنیم
+            txtDisplay.Text += ((Button)sender).Text;
+        }
+
+        private void Operators(object sender, MouseEventArgs e)
+        {
+            //برای چک کردن اینکه آیا عملگر بار زده شده یا بار دوم
+            if (op != null)
+            {
+                btnEqual_Click(null, null);
+            }
+            //عدد اول را که در جعبه متن وارد شده است ذخیره می کنیم
+            x = Convert.ToDouble(txtDisplay.Text);
+            //ذخیره کردن عملگر
+            op = ((Button)sender).Text;
+            flag = true;// تا مقدار عدد اول به همراه عملگر در حافظه ذخیره شود
+        }
+
+        private void btnEqual_Click(object sender, MouseEventArgs e)
+        {
+            //ذخیره عدد دوم
+            y = Convert.ToDouble(txtDisplay.Text);
+            //انجام عملیات محاسبه توسط تعیین نوع عملگر انتخاب شده
+            switch (op)
+            {
+                case "+":
+                    z = x + y;
+                    break;
+
+                case "-":
+                    z = x - y;
+                    break;
+
+                case "*":
+                    z = x * y;
+                    break;
+
+                case "/":
+                    z = x / y;
+                    break;
+            }
+            //نمایش نتیجه
+            txtDisplay.Text = z.ToString();
+            op = null;//بعد از اینکه عملیات انجام شد عملگر را خالی میکنیم
+        }
+
+        private void txtDisplay_TextChanged(object sender, EventArgs e)
+        {
+            //چک میکنیم وقتی بروی دکمه دات یکبار فشرده شد دکمه ممیز خاموش شود
+            btnDot.Enabled = !txtDisplay.Text.Contains(".");
+
+            //با استفاده از این متد طول متن صفحه نمایش را کنترل می کنیم تا درصورت خالی بودن با خطا مواجه نشویم
+            btnDel.Enabled = Convert.ToBoolean(txtDisplay.Text.Length);
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            //از اولین کاراکتر تا یکی کمتر از طول متن عمل حذف را انجام می دهیم Substringبا استفاده از متد 
+            if (txtDisplay.Text.Length > 0)
+            {
+                txtDisplay.Text = txtDisplay.Text.Substring(0, txtDisplay.Text.Length - 1);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //برای اینکه در هنگام اجرای اولیه برنامه صفحه نمایش خالی می باشد اجازه حذف کردن نداریم
+            //را صدا می زنیم txtChanged پس متد نوشته شده در رویداد
+            txtDisplay_TextChanged(null, null);
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //رخ دادی که برای شی فوکوس دار زمانی که کلیدی از صفحه کلید را فشار دهید اتفاق می افتد
+            //رای اینکه تمام دکمه های روی فرم این عمل را انجام دهند برای رخ داد فرم آن را فعال میکنیم
+            ///فرم فعال باشد تا کلید هارا ببینید Key Preview تکته باید خصوصیت
+
+            foreach (Button x in panel1.Controls)
+            {
+                ///برای اینکه وقتی از طریق کیبورد دکمه هارا فشار می دهیم عمل فوکوس و تغییر رنگ انجام دهد از حلقه استفاده میکنیم
+                if (x.Text == e.KeyChar.ToString())
+                {
+                    x.Focus();
+                    x.ForeColor = Color.Maroon;
+                }
+                else
+                {
+                    x.ForeColor = Color.Black;
+                }
+            }
+
+            Button temp = new Button();// ساخت شی از دکمه
+            temp.Text = e.KeyChar.ToString();
+
+            if (e.KeyChar >= '0' && e.KeyChar <= '9')
+            {
+                Numbers(temp, null);
+            }
+            else if (e.KeyChar == '+' || e.KeyChar == '-' || e.KeyChar == '*' || e.KeyChar == '/')
+            {
+                Operators(temp, null);
+            }
+            else if (e.KeyChar == '=')
+            {
+                btnEqual_Click(null, null);
+            }
+            else if (e.KeyChar == '.' && txtDisplay.Text.Contains(".") == false)//اجازه زدن بیشتر از یک میمز را در کیبورد نمی دهیم
+            {
+                Numbers(temp, null);
+            }
+            else if (e.KeyChar == '\b')
+            {
+                btnDel_Click(null, null);
+            }
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            //استفاده کنیم این رخ داد را فعال می کنیم Enter,f1-12 ,.... وقتی بخواهیم از دکمه های توسعه یافته مثل 
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnEqual_Click(null, null);
+                btnEqual.Focus();
+                btnEqual.ForeColor = Color.Red;
+            }
+        }
+
+        private void btnOnOff_Click(object sender, EventArgs e)
+        {
+            panel1.Enabled = !panel1.Enabled;
+            if (btnOnOff.Text == "On")
+            {
+                btnOnOff.Text = "Off";
+                this.KeyPreview = true;
+            }
+            else
+            {
+                btnOnOff.Text = "On";
+                this.KeyPreview = false;
+            }
         }
     }
 }
